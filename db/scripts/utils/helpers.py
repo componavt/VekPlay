f4cdd7253993c1bf2c5086d2befb23865a4ab0ad
componavt <andrew.krizhanovsky@gmail.com>
2025-11-25 23:36:53 +0300

    + db dumper

--- db/scripts/utils/helpers.py
+++ db/scripts/utils/helpers.py
@@ -22,3 +22,27 @@ def save_to_csv(dataframe, filepath):
     # Create directory if it doesn't exist
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
     dataframe.to_csv(filepath, index=False)
+
+
+def save_to_json(data, filepath):
+    """Save data to JSON file"""
+    # Create directory if it doesn't exist
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    
+    import json
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+def log_error(message):
+    """Log error message"""
+    import logging
+    logger = logging.getLogger(__name__)
+    logger.error(message)
+
+
+def log_info(message):
+    """Log info message"""
+    import logging
+    logger = logging.getLogger(__name__)
+    logger.info(message)
